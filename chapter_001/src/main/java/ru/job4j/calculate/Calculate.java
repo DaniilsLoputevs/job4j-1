9e84ad7ee9dfa931950ebd2da14e4cbf2e68474e
eveletspb <dinospb@gmail.com>
2018-05-25 21:44:55 +0300

    1.2. Подключить сборщик Maven[#4086]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,5 @@
-﻿package ru.job4j.calculate;
-	public class Calculate{
+package ru.job4j.calculate;
+	public class Calculate {
 		public static void main(String[]args){
 			System.out.println("Hello World");
 		}
