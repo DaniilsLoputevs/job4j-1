13a8e52a5e646063adc2cb42a7562d647bd2d3df
eveletspb <dinospb@gmail.com>
2018-05-27 13:43:22 +0300

    Create CalculateTest

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Sergey Bolshanin (dinospb@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+public class CalculateTest {
+/**
+* Test echo.
+*/ @Test
+public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "Sergey Bolshanin";
+    String expect = "Echo, echo, echo : Sergey Bolshanin"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+ 
+}
+ 
