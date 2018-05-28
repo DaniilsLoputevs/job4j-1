558ca4e641b017e2200ad1faa5c19c81753875b0
eveletspb <dinospb@gmail.com>
2018-05-28 19:55:53 +0300

    test git rm -rf target

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -5,23 +5,23 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Sergey Bolshanin (dinospb@gmail.com)
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Sergey Bolshanin (dinospb@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
-/**
-* Test echo.
-*/ @Test
-public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Sergey Bolshanin";
-    String expect = "Echo, echo, echo : Sergey Bolshanin"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
- 
+    /**
+     * Test echo.
+     */ @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Sergey Bolshanin";
+        String expect = "Echo, echo, echo : Sergey Bolshanin";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
 }
  
