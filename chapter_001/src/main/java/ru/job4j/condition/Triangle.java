4cdf051ef43d6cc6f46d77e1710342ee0451731b
eveletspb <dinospb@gmail.com>
2018-06-03 23:14:52 +0300

    Правка задания по вычислению площади треугольника

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -39,7 +39,8 @@ package ru.job4j.condition;
 /**
  * Проверка на условие : сумма двух сторон должна быть больше третьей стороны .
  */
-            return (ab + bc > ac) && (bc + ac > ab) && (ac + ab > bc);
+
+            return (ab + ac > bc);
         }
     }
 
