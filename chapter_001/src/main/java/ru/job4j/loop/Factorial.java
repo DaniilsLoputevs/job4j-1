297286fbbed250312a555d07bd9713f4eb87bf78
eveletspb <dinospb@gmail.com>
2018-06-05 21:56:53 +0300

    add Factorial add FactorialTest

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,10 @@
+package ru.job4j.loop;
+
+public class Factorial {
+    public int calc(int n) {
+     int fact = 1;
+     for (int i = fact; i <= n; i++) {
+         fact = fact * i;
+     } return fact;
+    }
+}
