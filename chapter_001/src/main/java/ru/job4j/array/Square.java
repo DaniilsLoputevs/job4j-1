178c9b9e6c240aa632d313e31d1abede67aae521
eveletspb <dinospb@gmail.com>
2018-06-12 20:38:25 +0300

    правка Square

--- chapter_001/src/main/java/ru/job4j/array/Square.java
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -3,24 +3,11 @@ package ru.job4j.array;
 public class Square {
     public int[] calculate(int bound) {
         int[]rst = new int[bound];
-        for (int index = 0; index <= bound; index++) {
-            if (index == 1) {
-                rst[0] = (int) Math.pow(index, 2);
-            }
-            if (index == 2) {
-                rst[1] = (int) Math.pow(index, 2);
-            }
-            if (index == 3) {
-                rst[2] = (int) Math.pow(index, 2);
+        for (int index = 0; index < bound; index++) {
+            rst[index] = (int) Math.pow((index + 1 ), 2);
 
-            }
-            if (index == 4) {
-                rst[3] = (int) Math.pow(index, 2);
-            }
-            if (index == 5) {
-                rst[4] = (int) Math.pow(index, 2);
-            }
 
-        } return rst;
+        }
+        return rst;
     }
 }
