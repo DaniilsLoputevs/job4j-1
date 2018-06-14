189ced1c3edc684f8981e8725fcbcbbf8c8b2d67
eveletspb <dinospb@gmail.com>
2018-06-14 20:52:47 +0300

    BubbleSort и BubbleSortTest

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -1,17 +1,20 @@
-/**package ru.job4j.array;
+package ru.job4j.array;
 
 public class BubbleSort {
-    public int [] sort (int[] array) {
-        int temp = 0;
-        for (int i = array.length - 1; i > 0; i--) {
+    public int[] sort(int[] array) {
+
+        for (int i = array.length - 1; i >= 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (array[j] > array[j + 1]) {
-                    temp = array[j];
+                   int temp = array[j];
+                   array[j] = array[j + 1];
+                   array[j + 1] = temp;
+
                 }
             }
-
         }
+        return array;
+
 
     }
 }
-*/
--- chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -1,14 +1,21 @@
-/** package ackage ru.job4j.array;
+package ru.job4j.array;
 
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class BubbleSortTest {
     @Test
     public void whenSortArrayWithTenElementsThenSortedArray() {
+
         //напишите здесь тест, проверяющий сортировку массива из 10 элементов методом пузырька, например {1, 5, 4, 2, 3, 1, 7, 8, 0, 5}.
+        BubbleSort sorter = new BubbleSort();
+        int[] input = new int[]{1, 5, 4, 2, 3, 1, 7, 8, 0, 5};
+        int[] result = sorter.sort(input);
+        int[] expect = new int[]{0, 1, 1, 2, 3, 4, 5, 5, 7, 8};
+        assertThat(result, is(expect));
+
+
     }
 }
- */
