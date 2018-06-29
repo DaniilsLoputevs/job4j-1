c394666f2e2453b31b9383665271a12e87e62e0f
eveletspb <dinospb@gmail.com>
2018-06-29 22:37:52 +0300

    Правка доп задания

--- chapter_001/src/main/java/ru/job4j/dop/Merger.java
+++ chapter_001/src/main/java/ru/job4j/dop/Merger.java
@@ -7,28 +7,22 @@ public class Merger {
         int firstIndex = 0;
         int secondIndex = 0;
         int i = 0;
-
         while (i < result.length) {
-            result[i] = a[firstIndex] < b[secondIndex] ? a[firstIndex++] : b[secondIndex++];
             if (firstIndex == a.length) {
-                for (int z = secondIndex; z < b.length; z++) {
-                    i++;
-                    result[i] = b[z];
-                }
-
-                break;
+                result[i] = b[secondIndex++];
+            } else if (secondIndex == b.length) {
+                result[i] = a[firstIndex++];
+            } else {
+                result[i] = a[firstIndex] < b[secondIndex] ? a[firstIndex++] : b[secondIndex++];
             }
-            if (secondIndex == b.length) {
-                for (int y = firstIndex; y < a.length; y++) {
-                    i++;
-                    result[i] = b[y];
-                }
 
-                break;
-            }
+
             i++;
         }
+
         return result;
     }
+
 }
+
 
