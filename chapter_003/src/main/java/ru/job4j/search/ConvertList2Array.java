7fe241315cec69717dac04b2309f62fba509792e
eveletspb <dinospb@gmail.com>
2018-11-26 22:36:55 +0300

    Конвертация листа массивов в один лист Integer

--- chapter_003/src/main/java/ru/job4j/search/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/search/ConvertList2Array.java
@@ -1,18 +1,22 @@
 package ru.job4j.search;
+
+import java.util.ArrayList;
 import java.util.List;
+
 /**
  * Package for Collections lite
  *
  * @author Sergey Bolshanin (dinospb@gmail.com)
  * @version $Id$
- * @since 21.11.2018
+ * @since 26.11.2018
  */
 
 public class ConvertList2Array {
     /**
      * Метод конвертирует List<Integer> в двумерный массив.
+     *
      * @param list- Список.
-     * @param rows - Строки.
+     * @param rows  - Строки.
      * @return - Возвращает равномерный двумерный массив , если элементов не хватает , оставшееся заполняется нулями.
      */
     public int[][] toArray(List<Integer> list, int rows) {
@@ -29,5 +33,22 @@ public class ConvertList2Array {
             }
         }
         return array;
+    }
+
+    /**
+     * Метод добавляется в List<Integer>, List<int[]> содержащий массив целых чисел.
+     *
+     * @param list Список с массивом целых чисел.
+     * @return List<Integer> с добавленными целыми числами .
+     */
+    public List<Integer> convert(List<int[]> list) {
+        List<Integer> result = new ArrayList<Integer>();
+        for (int[] ints : list) {
+            for (int integer : ints) {
+                result.add(integer);
+
+            }
+        }
+        return result;
     }
 }
--- chapter_003/src/test/java/ru/job4j/search/ConvertList2ArrayTest.java
+++ chapter_003/src/test/java/ru/job4j/search/ConvertList2ArrayTest.java
@@ -2,7 +2,9 @@ package ru.job4j.search;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
@@ -18,6 +20,19 @@ public class ConvertList2ArrayTest {
                 {4, 5, 6},
                 {7, 0, 0}};
         assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenListConvertInteger() {
+        ConvertList2Array list = new ConvertList2Array();
+        ArrayList<int[]> result = new ArrayList<>();
+        result.add(new int[]{1, 2});
+        result.add(new int[]{3, 4, 5, 6});
+        ArrayList<Integer> expect = new ArrayList<>();
+        for (int i = 1; i <= 6; i++) {
+            expect.add(i);
+        }
+        assertThat(list.convert(result), is(expect));
     }
 
 }
