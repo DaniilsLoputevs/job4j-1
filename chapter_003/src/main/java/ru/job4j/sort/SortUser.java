d61560760b4ea9c99be4332cca9115dfaaffdcd7
eveletspb <mailto:dinospb@gmail.com>
2019-02-06 21:01:18 +0300

    1. Организовать сортировку в классе UserSort по длинне и  лексикографически. 2. Удаление ошибочного блока из pom.xml

--- chapter_003/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sort/SortUser.java
@@ -1,5 +1,6 @@
 package ru.job4j.sort;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -22,6 +23,47 @@ public class SortUser {
         TreeSet<User> sorted = new TreeSet<>(list);
 
         return sorted;
+    }
+
+    /**
+     * Метод с реализацией Comparator по сортировке List по длинне имени.
+     *
+     * @param list Входящая структура данных.
+     * @return Отсортированная структура данных.
+     */
+    public List<User> sortNameLenght(List<User> list) {
+        list.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        return o1.getName().length() - o2.getName().length();
+                    }
+                }
+        );
+
+        return list;
+    }
+
+    /**
+     * Метод с реализацией Comparator по сортировке List по имени и возрасту.
+     *
+     * @param list Входящая структура данных.
+     * @return Отсортированная структура данных.
+     */
+    public List<User> sortByAllField(List<User> list) {
+        list.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        int res = o1.getName().compareTo(o2.getName());
+                        if (res == 0) {
+                            res = Integer.compare(o1.getAge(), o2.getAge());
+                        }
+                        return res;
+                    }
+                }
+        );
+        return list;
     }
 
 }
--- chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.sort;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -9,8 +10,29 @@ import java.util.TreeSet;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
+/**
+ * Тестирование методов класса SortUser .
+ */
 public class SortUserTest {
 
+    private final SortUser sortUser = new SortUser();
+    private final List<User> users = new ArrayList<>();
+    private final List<User> expected = new ArrayList<>();
+
+    @Before
+    public void initialArrays() {
+        users.add(new User("Bob", 19));
+        users.add(new User("Ernandes", 29));
+        users.add(new User("Alexander", 33));
+        users.add(new User("Anna", 5));
+        users.add(new User("Viktor", 2));
+
+
+    }
+
+    /**
+     * Тестирование метода возврата листа в TreeSet.
+     */
     @Test
     public void whenAllBeRight() {
         SortUser sortUser = new SortUser();
@@ -22,8 +44,37 @@ public class SortUserTest {
         users.add(new User("Andrew", 29));
         TreeSet<User> result = new TreeSet<>();
         result.addAll(users);
-        System.out.println(result);
         assertThat(result, is(sortUser.sort(users)));
     }
 
+    /**
+     * Тестирование метода сортировки по длинне имени.
+     */
+    @Test
+    public void whenSortedByLenght() {
+        expected.add(new User("Bob", 19));
+        expected.add(new User("Anna", 5));
+        expected.add(new User("Viktor", 2));
+        expected.add(new User("Ernandes", 29));
+        expected.add(new User("Alexander", 33));
+        assertThat(sortUser.sortNameLenght(users).toString(), is(expected.toString()));
+
+    }
+
+    /**
+     * Тестирование метода сортировки в лексикографическом порядке , если поле имя одинаковое , сортируется по поле возраст.
+     */
+    @Test
+    public void whenSortedByAllField() {
+        // добавление обьекта с одинаковым именем , проверка работы сортировке по возврасту.
+        users.add(new User("Alexander", 32));
+
+        expected.add(new User("Alexander", 32));
+        expected.add(new User("Alexander", 33));
+        expected.add(new User("Anna", 5));
+        expected.add(new User("Bob", 19));
+        expected.add(new User("Ernandes", 29));
+        expected.add(new User("Viktor", 2));
+        assertThat(sortUser.sortByAllField(users).toString(), is(expected.toString()));
+    }
 }
