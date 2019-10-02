eebf3c01ca65d42ce0d59d9bcf2c680ea1629fae
eveletspb <dinospb@gmail.com>
2019-10-02 23:01:15 +0300

    Калькулятор ООД[Consumer для вывода]

--- chapter_007/src/main/java/ru/job4j/ood/calc/calculatorood/MenuCalculator.java
+++ chapter_007/src/main/java/ru/job4j/ood/calc/calculatorood/MenuCalculator.java
@@ -3,44 +3,52 @@ package ru.job4j.ood.calc.calculatorood;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Consumer;
 
 public class MenuCalculator {
     private IterCalc iterCalc;
     private List<String> menu;
     private Scanner scanner;
     private ValidateInput validateInput;
+    private Consumer<String> output;
     boolean work;
 
-    public MenuCalculator(IterCalc iterCalc, ValidateInput validateInput) {
+    public MenuCalculator(IterCalc iterCalc, ValidateInput validateInput, Consumer<String> output) {
         this.iterCalc = iterCalc;
         this.menu = new ArrayList<>();
         this.scanner = new Scanner(System.in);
         this.validateInput = validateInput;
+        this.output = output;
         this.work = true;
     }
 
     public void show() {
-        this.menu.forEach(System.out::println);
+        this.menu.forEach(output);
     }
 
-    public void input() {
+    public void inputOutput() {
         String op;
+        Double f;
+        Double s;
+        Double result;
         do {
             System.out.println("Ввведите команду");
             op = scanner.next();
             if (this.validateInput.checkInputOperation(op)) {
-                System.out.println("введите первое число");
-                Double f = scanner.nextDouble();
-                System.out.println("введите второе число");
-                Double s = scanner.nextDouble();
-                System.out.println(this.iterCalc.operation(op, f, s));
+                this.output.accept("введите первое число");
+                f = scanner.nextDouble();
+                this.output.accept("введите второе число");
+                s = scanner.nextDouble();
+                result = this.iterCalc.operation(op, f, s);
+                output.accept(result.toString());
 
             } else if (this.validateInput.checkInputMemoryUse(op)) {
-                System.out.println("Введите команду");
+                output.accept("Введите команду");
                 op = scanner.next();
-                System.out.println("Введите число");
-                Double f = scanner.nextDouble();
-                System.out.println(this.iterCalc.operation(op, f));
+                output.accept("Введите число");
+                f = scanner.nextDouble();
+                result = this.iterCalc.operation(op, f);
+                output.accept(result.toString());
             }
 
 
@@ -62,10 +70,10 @@ public class MenuCalculator {
         validateInput.init();
         IterCalc iterCalc = new IterCalc();
         iterCalc.init();
-        MenuCalculator menuCalculator = new MenuCalculator(iterCalc, validateInput);
+        MenuCalculator menuCalculator = new MenuCalculator(iterCalc, validateInput, System.out::println);
         menuCalculator.init();
         menuCalculator.show();
-        menuCalculator.input();
+        menuCalculator.inputOutput();
     }
 
 }
