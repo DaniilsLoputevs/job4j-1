dac63f63a97b9910c5995db0eda6c53e18e7b5be
eveletspb <dinospb@gmail.com>
2019-10-13 20:40:13 +0300

    1.3 Cоздать Меню[Каркас задания]

--- chapter_007/src/main/java/ru/job4j/ood/menu/Adding.java
+++ chapter_007/src/main/java/ru/job4j/ood/menu/Adding.java
@@ -2,5 +2,5 @@ package ru.job4j.ood.menu;
 
 public interface Adding {
 
-    void insertSubMenu(String title);
+    void insertParent(String title);
 }
--- chapter_007/src/main/java/ru/job4j/ood/menu/Menu.java
+++ chapter_007/src/main/java/ru/job4j/ood/menu/Menu.java
@@ -1,21 +1,29 @@
 package ru.job4j.ood.menu;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Menu implements Adding {
     private String title;
     private Submenu submenu;
-    private List<Submenu> submenuList;
+    private Map<String, List<Submenu>> submenuListMap;
 
     public Menu(String title) {
         this.title = title;
+        this.submenuListMap = new TreeMap<>(String::compareTo);
     }
 
     @Override
-    public void insertSubMenu(String titlemenu) {
-        this.submenu = new Submenu();
+    public void insertParent(String titlemenu) {
+        submenu = new Submenu(title);
+        submenuListMap.put(submenu.title, new ArrayList<>());
 
+    }
 
+    public Map<String, List<Submenu>> getSubmenuListMap() {
+        return submenuListMap;
     }
 
     @Override
@@ -32,13 +40,19 @@ public class Menu implements Adding {
                 '}';
     }
 
-    private class Submenu implements Adding {
+    private class Submenu implements AddingSub {
         private String title;
         private List<String> submenu;
 
+        public Submenu(String title) {
+            this.title = title;
+        }
+
         @Override
-        public void insertSubMenu(String title) {
+        public void insertChildMenu(String title) {
 
         }
     }
+
+
 }
