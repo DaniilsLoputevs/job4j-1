2b4a9287ba290623dcb5a5860d4d7a440605a25a
eveletspb <dinospb@gmail.com>
2019-10-25 23:49:35 +0300

    4.Скачивание файла с ограничениями [переделал потоки]

--- chapter_008/src/main/java/ru/job4j/concurrency/Dispatcher.java
+++ chapter_008/src/main/java/ru/job4j/concurrency/Dispatcher.java
@@ -1,8 +1,13 @@
 package ru.job4j.concurrency;
 
+
 public class Dispatcher {
+
     public static void main(String[] args) {
-        Thread a = new Thread(new Downloader("http://releases.ubuntu.com/18.04/ubuntu-18.04.3-desktop-amd64.iso", 1000));
+        Dispatcher dispatcher = new Dispatcher();
+        String url = "http://releases.ubuntu.com/18.04/ubuntu-18.04.3-desktop-amd64.iso";
+        Downloader downloader = new Downloader(url, 500);
+        Thread a = new Thread(downloader);
         a.start();
     }
 }
--- chapter_008/src/main/java/ru/job4j/concurrency/Downloader.java
+++ chapter_008/src/main/java/ru/job4j/concurrency/Downloader.java
@@ -1,14 +1,14 @@
 package ru.job4j.concurrency;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.time.LocalTime;
 
 public class Downloader implements Runnable {
     private String url;
-    private int limit;
+    private File outfile;
+    private final int limit;
 
     public Downloader(String url, int limit) {
         this.url = url;
@@ -24,19 +24,56 @@ public class Downloader implements Runnable {
             filename = filename.substring(filename.lastIndexOf("/") + 1);
             HttpURLConnection h = (HttpURLConnection) url.openConnection();
             long sizefile = h.getContentLength();
-            byte[] buffer = new byte[4096];
             int r = 0;
-            File outfile = new File(System.getProperty("java.io.tmpdir") + File.separator + filename);
+            outfile = new File(System.getProperty("java.io.tmpdir") + File.separator + filename);
             try (InputStream in = h.getInputStream();
-                 FileOutputStream f = new FileOutputStream(outfile)) {
-                while ((r = in.read(buffer)) != -1) {
-                    f.write(buffer, 0, r);
+                 FileWriter f = new FileWriter(outfile)) {
+                while ((r = in.read()) != -1) {
+                    f.write(r);
+
                 }
 
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
+
+    private long checksize(long value, File f) {
+        return f.length() - value;
+    }
+
+    public File getOutfile() {
+        return outfile;
+    }
+
+//    private boolean sleepcondition(long filelenght) {
+//        int now = LocalTime.now().getSecond();
+//        int upd = 0;
+//        boolean rs = false;
+//        do {
+//            if (filelenght > limit) {
+//                rs = true;
+//                upd = LocalTime.now().getSecond();
+//            }
+//        } while (now - upd > -1);
+//        return rs;
+//    }
+
+
+    private long sizedownload(long source, long dest) {
+        long updsource = source - dest;
+        int now = LocalTime.now().getSecond();
+        int upd = 0;
+        long result = 0;
+        do {
+            result = (dest - updsource);
+            upd = LocalTime.now().getSecond();
+        } while (now - upd > -1);
+        return result;
+    }
+
+
 }
+//такт 1 4 мбайт - 2000мб = 1996мб
+//такт 2 8 мбайт - 2000мб = 199
