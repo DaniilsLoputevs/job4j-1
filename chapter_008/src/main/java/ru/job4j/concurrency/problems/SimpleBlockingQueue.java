e8b8f1a9f736474f3d99e748303125fdb44854ad
eveletspb <dinospb@gmail.com>
2019-11-03 22:23:14 +0300

    1. Реализовать шаблон Producer Consumer.[#191584][реализация самоблокирющейся очереди]

--- chapter_008/src/main/java/ru/job4j/concurrency/problems/SimpleBlockingQueue.java
+++ chapter_008/src/main/java/ru/job4j/concurrency/problems/SimpleBlockingQueue.java
@@ -1,17 +1,75 @@
 package ru.job4j.concurrency.problems;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
 import java.util.LinkedList;
 import java.util.Queue;
 
+@ThreadSafe
 public class SimpleBlockingQueue<T> {
+    @GuardedBy("this")
+    private Queue<T> queue;
+    private boolean canWork;
 
-    private Queue<T> queue = new LinkedList<>();
-
-    public void offer(T value) {
-
+    public SimpleBlockingQueue() {
+        this.queue = new LinkedList<>();
+        canWork = false;
     }
 
-    public T poll() {
-        return null;
+    public synchronized void offer(T value) {
+        while (canWork) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+        queue.offer(value);
+        canWork = true;
+        notify();
+        System.out.println("Ввведено значение в очередь " + value);
+    }
+
+    public synchronized T poll() {
+        T rs = null;
+        while (!canWork) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        rs = queue.poll();
+        canWork = false;
+        notify();
+        System.out.println("Значение из очереди " + rs);
+        return rs;
+    }
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread a = new Thread(() -> {
+            while (true) {
+                int i = 1;
+                queue.offer(i++);
+
+            }
+        });
+        Thread b = new Thread(() -> {
+            while (true) {
+                System.out.println(queue.poll());
+            }
+        });
+        a.start();
+        b.start();
+        try {
+            a.join();
+            b.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
